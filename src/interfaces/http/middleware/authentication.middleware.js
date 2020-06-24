4c5c316924e821db5881be5bbda8333c3204590b
Gustavo Rodarte <gustavo.rodarte@zrp.com.br>
2020-06-24 10:39:30 -0300

    fix authetication midleware

--- src/interfaces/http/middleware/authentication.middleware.js
+++ src/interfaces/http/middleware/authentication.middleware.js
@@ -2,17 +2,21 @@ const jwt = require('jsonwebtoken');
 const { Err } = require('crocks/Result');
 
 
-const decodedToken = (authorizationHeader) => {
+const decodedToken = (authorizationHeader, authSecret) => {
   const token = authorizationHeader.replace('Bearer ', '');
-  const decoded = jwt.verify(token, 'supersecret');
+  const decoded = jwt.verify(token, authSecret);
   return decoded;
 };
 
-module.exports = (req, requireAuth = true) => {
+module.exports = ({
+  config: {
+    authSecret,
+  },
+}) => (req, requireAuth = true) => {
   const header = req.req.headers.authorization;
 
   return header
-    ? decodedToken(header)
+    ? decodedToken(header, authSecret)
     : requireAuth
       ? Err('Login in to access resource')
       : {};
